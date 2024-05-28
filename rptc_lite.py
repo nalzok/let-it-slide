995ed7d8b823a184451188c371490d0169d2488e
Qingyao Sun <qs234@cornell.edu>
2024-05-28 17:22:37 +0000

    refactoring

--- rptc_lite.py
+++ rptc_lite.py
@@ -5,7 +5,6 @@ from functools import partial
 
 import jax
 import jax.numpy as jnp
-import jax.scipy as jsp
 import optax
 from tqdm import trange
 
@@ -17,62 +16,43 @@ def index_fn(x, L, S):
     return x
 
 
-def quantize(alphabet, L, S, corrections, samples, diag_only=True):
-    length = samples.shape[0]
+def quantize(alphabet, L, S, R, importance, samples):
+    length, V = samples.shape
+    maybe_length, maybe_V = importance.shape
+    assert maybe_length == length and maybe_V == V, (importance.shape, samples.shape)
+
+    assert jnp.iinfo(jnp.int32).bits % R == 0
+    pack_size = jnp.iinfo(jnp.int32).bits // R
+
     permutation = jnp.arange(1<<L)
     idx = index_fn(permutation, L, S)
     permuted_alphabet = alphabet[idx]
 
-    assert len(samples.shape) == 1, samples.shape
-    assert len(corrections.shape) == 2, corrections.shape
-    assert length == corrections.shape[0] == corrections.shape[1], (length, corrections.shape)
-
-    # checkify.check(jnp.allclose(jnp.tril(corrections), corrections), "corrections must be lower triangular")
-
     def viterbi_step(carry, index):
-        rho, prev_states = carry
-        prefixes = jnp.arange(1<<(L-2))
-        discarded = jnp.arange(1<<2)
-        last_state = (discarded<<(L-2)) | prefixes[..., jnp.newaxis]
+        rho, prev_msbs = carry
 
-        optimal_discarded = jnp.argmin(rho[last_state], axis=-1)
-        prev_state = (optimal_discarded<<(L-2)) | prefixes
-        updated_prev_states = prev_states.at[index].set(prev_state)
-        
-        if diag_only:
-            additive_loss = jnp.sum(corrections[index]**2) * (samples[index] - permuted_alphabet)**2
+        reshaped_rho = jnp.reshape(rho, (1<<R, 1<<(L-R)))
+        prev_msb = jnp.argmin(reshaped_rho, axis=-2)
+        pos, offset = divmod(index, pack_size)
+        updated_prev_msbs = prev_msbs.at[pos].add(prev_msb<<(offset*R))
 
-        else:
-            def end(val):
-                idx, state, error = val
-                return idx >= 0
+        additive_loss = jnp.sum(importance[index] * (samples[index]-permuted_alphabet)**2, axis=-1)
+        updated_rho = jnp.repeat(reshaped_rho[prev_msb, jnp.arange(1<<(L-R))], 1<<R) + additive_loss
 
-            def make_correction(val):
-                idx, state, error = val
-                updated_state = prev_states[idx, state>>2]
-                updated_error = error + corrections[index, idx] * (samples[idx] - permuted_alphabet[state])
-                return idx-1, updated_state, updated_error
-
-            init_val = (index, jnp.arange(1<<L), jnp.zeros((1<<L,)))
-            negative_one, zero, error = jax.lax.while_loop(end, make_correction, init_val)
-            # checkify.check(negative_one == -1, "negative_one should be -1, got {}", negative_one)
-            # checkify.check(zero == 0, "zero should be 0, got {}", zero)
-            additive_loss = error**2
-
-        updated_rho = jnp.repeat(rho[prev_state], 1<<2) + additive_loss
-
-        return (updated_rho, updated_prev_states), None
+        return (updated_rho, updated_prev_msbs), None
 
     rho = jnp.full((1<<L,), jnp.inf)
     rho = rho.at[0].set(0)  # initial state is zero (TODO: Tail-Biting)
-    prev_states = jnp.empty((length, 1<<(L-2)), dtype=int)
-    (rho, prev_states), _ = jax.lax.scan(viterbi_step, (rho, prev_states), jnp.arange(length))
+    prev_msbs = jnp.zeros(((length+pack_size-1)//pack_size, 1<<(L-R)), dtype=jnp.int32)
+    (rho, prev_msbs), _ = jax.lax.scan(viterbi_step, (rho, prev_msbs), jnp.arange(length))
 
-    def backtrack_input(state, prev_state):
-        return prev_state[state>>2], state & 0b11
+    def backtrack_input(state, index):
+        pos, offset = divmod(index, pack_size)
+        msb = (prev_msbs[pos, state>>R]>>(offset*R)) & ((1<<R)-1)
+        return msb<<(L-R) | state>>R, state & ((1<<R)-1)
 
     last_state = jnp.argmin(rho)
-    zero, quantized_rev = jax.lax.scan(backtrack_input, last_state, jnp.flipud(prev_states))
+    zero, quantized_rev = jax.lax.scan(backtrack_input, last_state, jnp.flipud(jnp.arange(length)))
     # checkify.check(zero == 0, "zero should be 0, got {}", zero)
     quantized = jnp.flipud(quantized_rev)
     expected_loss = jnp.min(rho)
@@ -80,9 +60,9 @@ def quantize(alphabet, L, S, corrections, samples, diag_only=True):
     return quantized, expected_loss
 
 
-def dequantize(alphabet, L, S, quantized):
+def dequantize(alphabet, L, S, R, quantized):
     def f(state, input_):
-        next_state = (state<<2) | input_
+        next_state = (state<<R) | input_
         idx = index_fn(next_state, L, S)
         output = alphabet[idx]
         return next_state, output
@@ -93,30 +73,27 @@ def dequantize(alphabet, L, S, quantized):
     return dequantized
     
 
-def evaluate(alphabet, L, S, corrections, samples):
-    quantized, _ = quantize(alphabet, L, S, corrections, samples)
-    dequantized = dequantize(alphabet, L, S, quantized)
+def evaluate(alphabet, L, S, R, importance, samples):
+    quantized, _ = quantize(alphabet, L, S, R, importance, samples)
+    dequantized = dequantize(alphabet, L, S, R, quantized)
     residual = samples - dequantized
-    mse = jnp.mean(residual**2)
+    mse = jnp.mean(importance * residual**2)
 
-    bincount = jnp.bincount(quantized, length=4)
+    bincount = jnp.bincount(quantized, length=1<<R)
     dist = bincount / jnp.sum(bincount)
     entropy = -jnp.sum(dist * jnp.log2(dist))
 
     return mse, entropy
 
 
-def train(key, alphabet, L, S, block_size, learning_rate, n_steps):
-
+def train(key, alphabet, L, S, R, V, T, importance, learning_rate, n_steps):
     @jax.jit
     def train_step(key_step, ab, opt_state):
+        samples = jax.random.normal(key_step, (T, V))
         grad_fn = jax.value_and_grad(evaluate, has_aux=True)
-        samples = jax.random.normal(key_step, (block_size,))
-        corrections = jnp.eye(block_size)
-        (mse, entropy), grads = grad_fn(ab, L, S, corrections, samples)
+        (mse, entropy), grads = grad_fn(ab, L, S, R, importance, samples)
         updates, opt_state = gradient_transform.update(grads, opt_state, ab)
         ab = optax.apply_updates(ab, updates)
-
         return mse, entropy, ab, opt_state
 
     scheduler = optax.warmup_cosine_decay_schedule(
@@ -131,7 +108,7 @@ def train(key, alphabet, L, S, block_size, learning_rate, n_steps):
     )
     opt_state = gradient_transform.init(alphabet)
 
-    for step in (pbar := trange(n_steps)):
+    for step in (pbar := trange(n_steps, leave=False)):
         key_step = jax.random.fold_in(key, step)
         mse, entropy, alphabet, opt_state = train_step(key_step, alphabet, opt_state)
         pbar.set_description(f"{mse.item() = :.4f}, {entropy.item() = :.4f}")
@@ -139,39 +116,35 @@ def train(key, alphabet, L, S, block_size, learning_rate, n_steps):
     return alphabet
 
 
-def main(L, S, block_size, learning_rate, n_steps):
+def main(L, S, R, V, T, learning_rate, n_steps):
     key = jax.random.PRNGKey(42)
-    key_train, key_test = jax.random.split(key)
-    alphabet_half = jsp.stats.norm.ppf(1/2 + 1/(1<<(S+1)) + jnp.arange(1<<(S-1))/(1<<S))
-    alphabet = jnp.concatenate([-jnp.flipud(alphabet_half), alphabet_half])
-    print("Before:", alphabet)
+    key_alphabet, key_train, key_test = jax.random.split(key, num=3)
 
-    samples = jax.random.normal(key_test, (2**20//block_size, block_size))
-    corrections = jnp.eye(block_size)
+    assert V == 2
+    alphabet = jax.random.normal(key_alphabet, (1<<S, V))
+    importance = jnp.ones((T, V))
 
-    mse_all, entropy_all = jax.lax.map(partial(evaluate, alphabet, L, S, corrections), samples)
-    mse_mean = jnp.mean(mse_all).item()
-    mse_std = jnp.std(mse_all).item()
-    entropy_mean = jnp.mean(entropy_all).item()
-    entropy_std = jnp.std(entropy_all).item()
-    print(f"Before: {mse_mean = :.4f} ({mse_std:.3f}), {entropy_mean = :.4f} ({entropy_std:.3f})")
+    alphabet_tuned = train(key_train, alphabet, L, S, R, V, T, importance, learning_rate, n_steps)
 
-    # fine-tine the alphabet
-    # alphabet = train(key_train, alphabet, L, S, block_size, learning_rate, n_steps)
-    # print("After:", alphabet)
-    #
-    # mse_all, entropy_all = jax.lax.map(partial(evaluate, alphabet, L, S, corrections), samples)
-    # mse_mean = jnp.mean(mse_all).item()
-    # mse_std = jnp.std(mse_all).item()
-    # entropy_mean = jnp.mean(entropy_all).item()
-    # entropy_std = jnp.std(entropy_all).item()
-    # print(f"After: {mse_mean = :.4f} ({mse_std:.3f}), {entropy_mean = :.4f} ({entropy_std:.3f})")
+    samples = jax.random.normal(key_test, (2**20//T, T, V))
+
+    mse_all, entropy_all = jax.lax.map(partial(evaluate, alphabet, L, S, R, importance), samples)
+    mse_mean, mse_std = jnp.mean(mse_all).item(), jnp.std(mse_all).item()
+    entropy_mean, entropy_std = jnp.mean(entropy_all).item(), jnp.std(entropy_all).item()
+    print(f"Original: {mse_mean = :.4f} ({mse_std:.3f}), {entropy_mean = :.4f} ({entropy_std:.3f})")
+
+    mse_all, entropy_all = jax.lax.map(partial(evaluate, alphabet_tuned, L, S, R, importance), samples)
+    mse_mean, mse_std = jnp.mean(mse_all).item(), jnp.std(mse_all).item()
+    entropy_mean, entropy_std = jnp.mean(entropy_all).item(), jnp.std(entropy_all).item()
+    print(f"Finetuned: {mse_mean = :.4f} ({mse_std:.3f}), {entropy_mean = :.4f} ({entropy_std:.3f})")
 
 
 if __name__ == "__main__":
     L = 16
-    S = 6
-    block_size = 2**10
-    learning_rate = 1e-2
+    S = 9
+    R = 4
+    V = 2
+    T = 2**10
+    learning_rate = 1e-3
     n_steps = 2**10
-    main(L, S, block_size, learning_rate, n_steps)
+    main(L, S, R, V, T, learning_rate, n_steps)
